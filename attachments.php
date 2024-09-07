6a550c9906a91966519e5068b3044270d7a63239
hudsam <app.hoedhya@gmail.com>
2024-09-07 19:12:46 +0700

    added button & modal

--- attachments.php
+++ attachments.php
@@ -5,7 +5,6 @@
 
     include './template/header.php';
 ?>
-
 <div class="container-fluid">
     <div class="table-responsive">
         <table id="example" class="table table-striped table-hover display" style="width:100%">
@@ -21,11 +20,11 @@
             <tbody>
                 <?php while($DataTemp = mysqli_fetch_assoc($ResultQuery)): ?>
                 <tr>
-                    <td title="<?php echo $DataTemp['ID']; ?>"><?php echo $DataTemp['display_name']; ?></td>
+                    <td><?php echo $DataTemp['display_name']; ?></td>
                     <td style="text-align: center;"><?php echo $DataTemp['post_date']; ?></td>
                     <td><?php echo $DataTemp['post_title']; ?> | <a href="<?php echo $DataTemp['guid']; ?>" target="_blank"><i class="bi bi-box-arrow-up-right"></i></a></td>
                     <td><?php echo $DataTemp['post_mime_type']; ?></td>
-                    <td></td>
+                    <td><button type="button" class="btn btn-sm btn-outline-dark button-ChangeUploadOn" data-bs-toggle="modal" data-bs-target="#modal-ChangeUploadOn" id="button-ChangeUploadOn" data-href="./edit-attachment.php?id=<?php echo $DataTemp['ID']; ?>"><i class="bi bi-pencil-square"></i> Uploaded On</button></td>
                 </tr>
                 <?php endwhile; ?>
             </tbody>
@@ -42,6 +41,12 @@
     </div>
 </div>
 
+<div class="modal fade" id="modal-ChangeUploadOn" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-md modal-dialog-centered">
+        <div class="modal-content">
+        </div>
+    </div>
+</div>
 <?php
     include './template/footer.php';
 ?>
