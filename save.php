78f0c69c45eca2b53e193f9be3cbc0467def0932
hudsam <app.hoedhya@gmail.com>
2024-09-07 20:12:52 +0700

    added prevent xss

--- save.php
+++ save.php
@@ -2,13 +2,13 @@
     require_once './database.php';
 
     $SAVE_Attachment = $_POST['submit'];
-    $SAVE_Attachment = ($SAVE_Attachment == 'save_UploadedOn' ? true : false);
+    $SAVE_Attachment = (htmlspecialchars($SAVE_Attachment, ENT_QUOTES, 'UTF-8') == 'save_UploadedOn' ? true : false);
 
-    $POST_ID = $_POST['ID'];
-    $POST_display_name = $_POST['display_name'];
-    $POST_post_date = explode('T', $_POST['post_date'])[0] . ' ' . explode('T', $_POST['post_date'])[1];
-    $POST_post_title = $_POST['post_title'];
-    $POST_post_mime_type = $_POST['post_mime_type'];
+    $POST_ID = htmlspecialchars($_POST['ID'], ENT_QUOTES, 'UTF-8');
+    $POST_display_name = htmlspecialchars($_POST['display_name'], ENT_QUOTES, 'UTF-8');
+    $POST_post_date = htmlspecialchars(explode('T', $_POST['post_date'])[0], ENT_QUOTES, 'UTF-8') . ' ' . htmlspecialchars(explode('T', $_POST['post_date'])[1], ENT_QUOTES, 'UTF-8');
+    $POST_post_title = htmlspecialchars($_POST['post_title'], ENT_QUOTES, 'UTF-8');
+    $POST_post_mime_type = htmlspecialchars($_POST['post_mime_type'], ENT_QUOTES, 'UTF-8');
 
     $Query = 'UPDATE `_posts` SET `post_date`= "'. $POST_post_date .'" WHERE `ID` = '. $POST_ID .';';
     mysqli_query($MySQLi, $Query);
