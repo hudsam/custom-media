de642220ae93e7d7c82d3f08e15857dbd5b3b837
hudsam <app.hoedhya@gmail.com>
2024-09-07 12:49:43 +0700

    added bootstrap 5.*.*

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,11 @@
+</body>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.datatables.net/2.1.5/js/dataTables.js"></script>
 <script>
-    $('#example').DataTable();
+    $(document).ready(function() {
+        $('#example').DataTable({
+
+        })
+    })
 </script>
+</html>
--- template/header.php
+++ template/header.php
@@ -1,1 +1,12 @@
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/2.1.5/css/dataTables.dataTables.css">
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/2.1.5/css/dataTables.dataTables.css">
+    <style>
+        th { text-align: center !important; }
+    </style>
+</head>
+<body>
