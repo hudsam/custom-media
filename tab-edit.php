aaddcc040a1b20f271c1e26aeb45a36b7ec5ae2e
hudsam <app.hoedhya@gmail.com>
2024-10-15 03:29:04 +0700

    added back page

--- tab-edit.php
+++ tab-edit.php
@@ -21,11 +21,12 @@
         <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item">Beranda</li>
-                <li class="breadcrumb-item">Media</li>
+                <li class="breadcrumb-item"><a href="./list.php">Daftar Media</a></li>
                 <li class="breadcrumb-item active" aria-current="page"><?php echo $DataTemp['post_title']; ?></li>
             </ol>
         </nav>
     </div>
+    <a href="./list.php" class="btn btn-sm btn-outline-secondary mb-3"><i class="bi bi-arrow-left-short"></i> Halaman Kembali</a>
     <div class="row">
         <div class="col-4">
             <form class="g-3" method="post" action="./save.php">
